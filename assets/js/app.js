5014644d2e262ff3bb8dd64109b312a6ca03a05d
Mohit <mkjakhar909@gmail.com>
2024-04-30 21:22:27 +0530

    mistakes improved

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,3 +10,63 @@ function showPassword() {
     inputType.type = "password";
   }
 }
+
+// table data
+var dataArray = [
+  {
+    id: 26166,
+    name: "Mark Nagelvoort",
+    department: "Finance",
+    status: "New",
+    category: "Vehicle Sale",
+    date: "12/10/2023",
+    vehicle: "1GXUDEE80DHZZUA",
+  },
+  {
+    id: 26161,
+    name: "Customer 2",
+    department: "Cash",
+    status: "Used",
+    category: "Vehicle Sale",
+    date: "12/10/2023",
+    vehicle: "QEVRUDEE80D778S",
+  },
+  {
+    id: 26166,
+    name: "Mark Nagelvoort",
+    department: "Finance",
+    status: "New",
+    category: "Vehicle Sale",
+    date: "12/10/2023",
+    vehicle: "1GXUDEE80DHZZUA",
+  },
+];
+
+// Function to populate the template with data
+function populateTemplate(data) {
+  var template = document.querySelector("#template").content.cloneNode(true);
+
+  template.querySelector(".fw-medium:nth-child(1)").textContent = data.id;
+  template.querySelector(".fw-medium:nth-child(2)").textContent = data.name;
+  template.querySelector(".fw-medium:nth-child(3)").textContent =
+    data.department;
+  template.querySelector(".fw-medium:nth-child(4)").textContent = data.status;
+  template.querySelector(".fw-medium:nth-child(5)").textContent = data.category;
+  template.querySelector(".fw-medium:nth-child(6)").textContent = data.date;
+  template.querySelector(".fw-medium:nth-child(7)").textContent = data.vehicle;
+
+  return template;
+}
+
+// Function to add populated templates to the DOM
+function renderData(dataArray) {
+  var container = document.getElementById("table_container");
+
+  dataArray.forEach(function (item) {
+    var template = populateTemplate(item);
+    container.appendChild(template);
+  });
+}
+
+// Call renderData function with the sample data array
+renderData(dataArray);
